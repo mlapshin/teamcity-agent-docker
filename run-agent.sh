0ee8572a9683cb101483e96e2ff35863db564d40
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-08-26 18:17:47 +0400

    fix fix fix

--- run-agent.sh
+++ run-agent.sh
@@ -6,6 +6,6 @@ cp /var/build-agent/conf/buildAgent.dist.properties $AGENT_CONFIG
 
 sed -i.bak "s@serverUrl=http://localhost:8111/@serverUrl=$1@g" $AGENT_CONFIG
 sed -i.bak "s@name=@name=$2@g" $AGENT_CONFIG
-sed -i.bak "s@ownPort=9090@name=$3@g" $AGENT_CONFIG
+sed -i.bak "s@ownPort=9090@ownPort=$3@g" $AGENT_CONFIG
 
 /var/build-agent/bin/agent.sh run
